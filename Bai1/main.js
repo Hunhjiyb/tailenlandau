da1015c4710fc745a3027a297d19bcba57366839
hung <quochung122402@gmail.com>
2022-07-01 08:35:04 +0700

    first commit

---
+++ Bai1/main.js
@@ -0,0 +1,76 @@
+ var n = Number(prompt('Nhap vao n:'))
+if (n == 2) {
+    document.write("<font color=red><b>BANG CUU CHUONG 2");
+    document.write("<font color=blue>");
+
+    for (j = 1; j <= 9; j++) {
+
+        document.write("<br>", j, " x 2 = ", n * j);
+    }
+
+}
+if (n == 3) {
+    document.write("<font color=red><b>BẢNG CỬU CHƯƠNG 3");
+    document.write("<font color=blue>");
+
+    for (j = 1; j <= 9; j++) {
+
+        document.write("<br>", " 3 x ", j, "=", n * j);
+    }
+
+}
+if (n == 4) {
+    document.write("<font color=red><b>BANG CUU CHUONG 4");
+    document.write("<font color=blue>");
+    for (j = 1; j <= 9; j++) {
+
+        document.write("<br>", j, " x 4 = ", n * j);
+    }
+
+}
+if (n == 5) {
+    document.write("<font color=red><b>BANG CUU CHUONG 5");
+    document.write("<font color=blue>");
+    for (j = 1; j <= 9; j++) {
+
+        document.write("<br>", j, " x 5 = ", n * j);
+    }
+
+}
+else
+    if (n == 6) {
+        document.write("<font color=red><b>BANG CUU CHUONG 6");
+        document.write("<font color=blue>");
+        for (j = 1; j <= 9; j++) {
+
+            document.write("<br>", j, " x 6 = ", n * j);
+        }
+
+    }
+if (n == 7) {
+    document.write("<font color=red><b>BANG CUU CHUONG 7");
+    document.write("<font color=blue>");
+    for (j = 1; j <= 9; j++) {
+
+        document.write("<br>", j, " x 7 = ", n * j);
+    }
+
+}
+if (n == 8) {
+    document.write("<font color=red><b>BANG CUU CHUONG 8");
+    document.write("<font color=blue>");
+    for (j = 1; j <= 9; j++) {
+
+        document.write("<br>", j, " x 8 = ", n * j);
+    }
+
+}
+if (n == 9) {
+    document.write("<font color=red><b>BANG CUU CHUONG 9</font>");
+    document.write("<font color=blue>");
+    for (j = 1; j <= 9; j++) {
+
+        document.write("<br>", j, " x 9 = ", n * j);
+    }
+
+}
